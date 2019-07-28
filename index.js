a965be0bbf89aec087ac312b3e435623b2b0dd0b
Phyuthazinthant <phyuthazin.mg@gmail.com>
2019-07-28 20:45:43 +0630

    third commit

--- index.js
+++ index.js
@@ -20,31 +20,31 @@ app.get('/', (req, res) => res.send("hell0 "));
 
 app.get('/users', (req, res) => {
 con.connect(function (err) {
-//if (err) throw err;
+if (err) throw err;
 con.query("SELECT * FROM test", function (err, result, fields) {
-//if (err) throw err;
+if (err) throw err;
 res.status(200).json(result);
 });
 });
 });
-
+con.end();
 app.post('/user/add', (req, res) => {
 data = req.body;
 con.connect(function (err) {
-//if (err) throw err;
+if (err) throw err;
 console.log("Connected!");
 var sql = `INSERT INTO test (id,name,age,job,address) VALUES ('${data.id}','${data.name}','${data.age}','${data.job}','${data.address}')`;
 con.query(sql, function (err, result) {
-//if (err) throw err;
+if (err) throw err;
 res.status(201).send("User created!");
 });
 });
 });
-
+con.end();
 app.put('/user/update/:uid', function (req, res) {
 id = req.params.uid;
 con.query('UPDATE `test` SET `name`=?,`age`=?,`job`=?,`address`=? where `id`=?', [req.body.name, req.body.age, req.body.job, req.body.address, id], function (error, results, fields) {
-//if (error) throw error;
+if (error) throw error;
 res.end(JSON.stringify(results));
 });
 });
@@ -53,7 +53,7 @@ app.delete('/user/remove/:uid', (req, res) => {
 id = req.params.uid;
 var sql = `DELETE FROM test WHERE id = ${id}`;
 con.query(sql, function (err, result) {
-//if (err) throw err;
+if (err) throw err;
 if (result.affectedRows == 0) {
 res.status(404).send("Not Found!");
 } else {
